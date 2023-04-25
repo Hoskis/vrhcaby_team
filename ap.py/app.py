036d3965bc6af2b1b805ebeeba56021af8204e69
Radim Havlik <radim52525@seznam.cz>
2023-04-25 21:54:33 +0200

    Skladani figurek na sebe

--- ap.py/app.py
+++ ap.py/app.py
@@ -12,32 +12,48 @@ def pohyb(bile, cerne):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-
+            #první kliknutí(výběr figurky)
             elif event.type == pygame.MOUSEBUTTONDOWN and kliknuti == 0:
                 pos = pygame.mouse.get_pos()
+                rada = polohovani.shoda(int(pos[0]),int(pos[1]))
+                zaznam = sdilene_polozky.databaze
                 for circle in bile + cerne:
-                    if circle.collidepoint(pos):
+                    if circle.collidepoint(zaznam[-1]):
                         vybrane = circle
+                        kolecko = pos
                         kliknuti = 1
                         break
+            #druhé kliknutí(přesun figurky)
             elif event.type == pygame.MOUSEBUTTONDOWN and kliknuti != 0:
                 souradnice = pygame.mouse.get_pos()
+                #funkce na určení nového pole
+                print(zaznam)
                 prvni = polohovani.shoda(int(souradnice[0]),int(souradnice[1]))
+                databaze_zaznamu = sdilene_polozky.databaze
+                print(databaze_zaznamu)
                 if vybrane:
                     pos = prvni
                     while pos in sdilene_polozky.databaze:
-                        poradi = len(sdilene_polozky.databaze)-1
-                        if pos[1] < 300:
-                            prvni[1]+= poradi*40
-                        else:
-                            prvni[1]-= poradi*40    
-                        pos = prvni
-                        if pos in sdilene_polozky.databaze:
+                        if rada == pos:
+                            pos = None
+                            kliknuti = 0
                             break
-                    sdilene_polozky.databaze.append(pos)
-                    vybrane.center = pos
+                        else:
+                            poradi = len(sdilene_polozky.databaze)-1
+                            if pos[1] < 300:
+                                prvni[1]+= poradi*40
+                            else:
+                                prvni[1]-= poradi*40    
+                            pos = prvni
+                            if pos in sdilene_polozky.databaze:
+                                break
+                    if pos != None:
+                        zaznam.pop(-1)
+                        sdilene_polozky.databaze.append(pos)
+                        vybrane.center = pos
                     vybrane = None
                     kliknuti = 0
+                    
 
         window.blit(pozadi, (0, 0))
         for circle in bile:
--- ap.py/polohovani.py
+++ ap.py/polohovani.py
@@ -3,99 +3,99 @@ import sdilene_polozky
 def shoda(poloha_x,poloha_y):
         if poloha_y < 300:
                 if 85 <= poloha_x <= 135:
-                        prvni = [109,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data1
+                        prvni = [109,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data13
                         #136-185 =2
                 elif (poloha_x >= 136) and (poloha_x <= 185):
-                        prvni = [159,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data2
+                        prvni = [159,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data14
                         #186-235 =3
                 elif (poloha_x >= 186) and (poloha_x <= 235):
-                        prvni = [209,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data3
+                        prvni = [209,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data15
                         #236-285 =4
                 elif (poloha_x >= 236) and (poloha_x <= 285):
-                        prvni = [259,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data4
+                        prvni = [259,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data16
                         #286-335 =5
                 elif (poloha_x >= 286) and (poloha_x <=335):
-                        prvni = [309,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data5
+                        prvni = [309,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data17
                         #336-383 =6
                 elif (poloha_x >= 336) and (poloha_x <= 383):
-                        prvni = [359,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data6
+                        prvni = [359,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data18
                         #417-467 =7
                 elif (poloha_x >= 417) and (poloha_x <= 467):
-                        prvni = [443,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data7
+                        prvni = [443,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data19
                         #468-517 =8
                 elif (poloha_x >= 468) and (poloha_x <= 517):
-                        prvni = [493,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data8
+                        prvni = [493,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data20
                         #518-567 =9
                 elif (poloha_x >= 518) and (poloha_x <= 567):
-                        prvni = [543,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data9
+                        prvni = [543,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data21
                         #568-617 =10
                 elif (poloha_x >= 568) and (poloha_x <= 617):
-                        prvni = [593,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data10
+                        prvni = [593,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data22
                         #618-667 =11
                 elif (poloha_x >= 618) and (poloha_x <= 667):
-                        prvni = [643,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data11
+                        prvni = [643,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data23
                         #668-717 =12
                 elif (poloha_x >= 668) and (poloha_x <= 717):
-                        prvni = [693,55]
-                        sdilene_polozky.databaze = sdilene_polozky.data12
+                        prvni = [693,50]
+                        sdilene_polozky.databaze = sdilene_polozky.data24
         else:
                 if 85 <= poloha_x <= 135:
-                        prvni = [109,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data1
+                        prvni = [109,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data12
                         #136-185 =2
                 elif (poloha_x >= 136) and (poloha_x <= 185):
-                        prvni = [159,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data2
+                        prvni = [159,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data11
                         #186-235 =3
                 elif (poloha_x >= 186) and (poloha_x <= 235):
-                        prvni = [209,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data3
+                        prvni = [209,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data10
                         #236-285 =4
                 elif (poloha_x >= 236) and (poloha_x <= 285):
-                        prvni = [259,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data4
+                        prvni = [259,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data9
                         #286-335 =5
                 elif (poloha_x >= 286) and (poloha_x <=335):
-                        prvni = [309,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data5
+                        prvni = [309,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data8
                         #336-383 =6
                 elif (poloha_x >= 336) and (poloha_x <= 383):
-                        prvni = [359,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data6
+                        prvni = [359,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data7
                         #417-467 =7
                 elif (poloha_x >= 417) and (poloha_x <= 467):
-                        prvni = [443,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data7
+                        prvni = [443,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data6
                         #468-517 =8
                 elif (poloha_x >= 468) and (poloha_x <= 517):
-                        prvni = [493,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data8
+                        prvni = [493,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data5
                         #518-567 =9
                 elif (poloha_x >= 518) and (poloha_x <= 567):
-                        prvni = [543,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data9
+                        prvni = [543,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data4
                         #568-617 =10
                 elif (poloha_x >= 568) and (poloha_x <= 617):
-                        prvni = [593,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data10
+                        prvni = [593,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data3
                         #618-667 =11
                 elif (poloha_x >= 618) and (poloha_x <= 667):
-                        prvni = [643,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data11
+                        prvni = [643,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data2
                         #668-717 =12
                 elif (poloha_x >= 668) and (poloha_x <= 717):
-                        prvni = [693,550]
-                        sdilene_polozky.databaze = sdilene_polozky.data12
+                        prvni = [693,540]
+                        sdilene_polozky.databaze = sdilene_polozky.data1
         return prvni
 
